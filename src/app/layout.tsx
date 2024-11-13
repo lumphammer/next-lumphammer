415658884d856ed518a27c03904cbad7042781ba
Neil de Carteret <git@lumphammer.net>
2024-11-13 10:34:22 +0000

    make glow text effect much sexier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,41 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <svg width="0" height="0" viewBox="0 0 600 200">
+          <defs>
+            <filter id="dropshadow">
+              <feGaussianBlur
+                in="SourceGraphic"
+                result="blurOut"
+                stdDeviation="18"
+              />
+
+              <feComposite
+                operator="over"
+                in="SourceGraphic"
+                in2="blurOut"
+                result="final"
+              />
+            </filter>
+            <filter id="gradient-overlay">
+              <feImage
+                href="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciPjxsaW5lYXJHcmFkaWVudCBpZD0iZyIgeDI9IjEiPjxzdG9wIHN0b3AtY29sb3I9InZhcigtLWx1bXBoYW1tZXItbWFyay1jb2xvci0xKSIvPjxzdG9wIG9mZnNldD0iMSIgc3RvcC1jb2xvcj0idmFyKC0tbHVtcGhhbW1lci1tYXJrLWNvbG9yLTIpIi8+PC9saW5lYXJHcmFkaWVudD48cmVjdCB3aWR0aD0iMTAwJSIgaGVpZ2h0PSIxMDAlIiBmaWxsPSJ1cmwoI2cpIi8+PC9zdmc+"
+                x="0"
+                y="0"
+                width="100%"
+                height="100%"
+                preserveAspectRatio="none"
+                result="gradient"
+              />
+              <feBlend
+                mode="overlay"
+                in="SourceGraphic"
+                in2="gradient"
+                result="gradientOverlay"
+              />
+            </filter>
+          </defs>
+        </svg>
         <header>
           <div className="container">
             <div className="logotype">
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -8,7 +8,7 @@ export default function Portfolio() {
       <div className={styles.grid}>
         <article>
           <h2>
-            <mark>GUMSHOE for Foundry VTT</mark>
+            <mark>GUMSHOE</mark> for Foundry VTT
           </h2>
           <p>
             <a href="https://github.com/lumphammer/gumshoe-fvtt">Website</a>
@@ -17,22 +17,52 @@ export default function Portfolio() {
             <strong>
               The Official GUMSHOE system for Foundry Virtual Tabletop.
             </strong>
-            Features character sheets, skill tests, and resource tracking.
+          </p>
+          <p>
+            This projects was started as personal/fan project in 2021. Pelgrane
+            Press has since blessed it as the official GUMSHOE system for
+            Foundry Virtual Tabletop.
+          </p>
+          <p>
+            It&apos;s almost a meta-system - a toolbox that can be used to
+            support anything using the core GUMSHOE engine. But it also ships
+            with one-click support for all the main GUMSHOE games, like Trail of
+            Cthulhu, Night&apos;s Black Agents, and Mutant City Blues.
           </p>
         </article>
         <article>
           <h2>
-            <mark>Trail of Cthulhu 2nd Edition for Foundry VTT</mark>
+            <mark>Trail of Cthulhu 2nd Edition Quickstart</mark> for Foundry VTT
           </h2>
           <p>
-            A system implementation of the Trail of Cthulhu 2nd Edition
-            roleplaying game system for the Foundry Virtual Tabletop platform.
-            Features character sheets, skill tests, and resource tracking.
+            To support their{" "}
+            <a href="https://www.backerkit.com/c/projects/pelgrane-press/trail-of-cthulhu-2nd-edition">
+              2024 crowdfunding campaign
+            </a>{" "}
+            for Trail of Cthulhu 2<sup>nd</sup> Edition, Pelgrane Press asked me
+            to create a Foundry module adaptation of the Quickstart rules. This
+            includes:
           </p>
+          <ul>
+            <li>
+              A new character sheet theme, built to match the &quot;trade
+              dress&quot; of the official print character sheet.
+            </li>
+            <li>
+              The text and artwork of the Quickstart PDF, converted into a
+              Foundry Journal will full internal hyperlinking.
+            </li>
+            <li>
+              A hyperlinked version of the Quickstart adventure, &quot;Stranger
+              Shores&quot;.
+            </li>
+            <li>A roster of pre-generated player characters, ready to play.</li>
+            <li>Thematic scene map made from freely-available materials.</li>
+          </ul>
         </article>
         <article>
           <h2>
-            <mark>This Website?</mark>
+            This very <mark>Website</mark> right here
           </h2>
           <p>
             A static website built with Next.js and PicoCSS. Features a
@@ -41,7 +71,7 @@ export default function Portfolio() {
         </article>
         <article>
           <h2>
-            <mark>Pub Crusade for Foundry VTT</mark>
+            <mark>Pub Crusade</mark> for Foundry VTT
           </h2>
           <p>
             A system implementation of the Pub Crusade roleplaying game system
