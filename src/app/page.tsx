3d58f2430a314d8bf26fce7d42083a6be92e2c10
Neil de Carteret <git@lumphammer.net>
2024-11-15 13:34:52 +0000

    home page fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 // import Image from "next/image";
 import Link from "next/link";
-import styles from "./page.module.scss";
+import styles from "./home.module.scss";
 import TripleChevron from "@/components/TripleChevron";
 // import Link from "next/link";
 
