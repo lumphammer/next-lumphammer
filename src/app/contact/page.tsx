afb1349de676b15e950b6bef73f01d1dfbad5eeb
Neil de Carteret <git@lumphammer.net>
2024-11-16 12:39:21 +0000

    contact options

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,5 @@
+import { FaEnvelope, FaGithub, FaBluesky } from "react-icons/fa6";
+
 export default function Contact() {
   return (
     <main className="container">
@@ -11,22 +13,23 @@ export default function Contact() {
 
         <div>
           <div>
-            <svg
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              height={24}
-              width={24}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
-              />
-            </svg>
+            <FaEnvelope />
             {"  "}
             <a href="mailto:contact@lumphammer.com">contact@lumphammer.com</a>
+          </div>
+          <div>
+            <FaGithub />
+            {"  "}
+            <a target="_blank" href="https://github.com/lumphammer">
+              github.com/lumphammer
+            </a>
+          </div>
+          <div>
+            <FaBluesky />
+            {"  "}
+            <a target="_blank" href="https://bsky.app/profile/lumphammer.com">
+              bsky.app/profile/lumphammer.com
+            </a>
           </div>
 
           {/* <div>
