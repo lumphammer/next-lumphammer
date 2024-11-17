3ca6fbc0757dcc5448054a397079c191bd3f985c
Neil de Carteret <git@lumphammer.net>
2024-11-17 13:08:02 +0000

    work on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,23 @@
 import Link from "next/link";
 import styles from "./home.module.scss";
 import TripleChevron from "@/components/TripleChevron";
+import Logo from "@/assets/Logo.svg";
+
 // import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="container">
       <div className={styles.hook}>
+        <Logo className={styles.logo} />
+
         <h1>
           <mark>Bespoke software development for the TTRPG industry</mark>
         </h1>
         <p className={styles.intro}>
-          Hi! We&apos;re a husband & wife team doing{" "}
+          Hi! <mark>Lumphammer</mark> is a husband & wife team offering{" "}
           <mark>software development</mark> and <mark>VTT conversions</mark> for
-          the TTRPG industry. conversions for the TTRPG industry.
+          the TTRPG industry.
         </p>
         <ul className={styles.links}>
           <li>
